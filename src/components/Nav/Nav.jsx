c56996edd97b0fbae691b59234bb9fd54987b2c9
Peter Ingles <peter.ingles@gmail.com>
2023-09-05 17:25:41 -0500

    NavBar Starter

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,45 +3,74 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div className="nav">
+        <Link to="/home">
+          <h2 className="nav-title">Prime Solo Project</h2>
         </Link>
+        <div>
+          {/* If no user is logged in, show these links */}
+          {!user.id && (
+            // If there's no user, show login/registration links
+            <Link className="navLink" to="/login">
+              Login / Register
+            </Link>
+          )}
+
+          {/* If a user is logged in, show these links */}
+          {user.id && (
+            <>
+              <Link className="navLink" to="/user">
+                Home
+              </Link>
+
+              <Link className="navLink" to="/info">
+                Info Page
+              </Link>
+
+              <LogOutButton className="navLink" />
+            </>
+          )}
+
+          <Link className="navLink" to="/about">
+            About
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
 
 export default Nav;
